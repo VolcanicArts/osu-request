0a735d9c30d0a4ab9372bbea2083c94107c8ecbb
VolcanicArts <VolcanicArtsBP@Gmail.com>
2021-12-21 20:02:59 -0600

    Add initial application test child

--- osu-request/Application.cs
+++ osu-request/Application.cs
@@ -1,6 +1,11 @@
 ﻿using osu.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu_request.Drawables;
 using osu_request.Osu;
 using osu_request.Twitch;
+using osuTK;
+using osuTK.Graphics;
 using volcanicarts.osu.NET.Client;
 
 namespace osu_request
@@ -14,6 +19,18 @@ namespace osu_request
         {
             _twitchClient = new TwitchClientLocal(true);
             _osuClient = new OsuClientLocal();
+        }
+
+        [BackgroundDependencyLoader]
+        private void Load()
+        {
+            Child = new SpinBox
+            {
+                Size = new Vector2(150, 150),
+                Colour = Color4.Red,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            };
         }
 
         protected override async void LoadAsyncComplete()
