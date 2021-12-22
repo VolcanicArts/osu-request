b4348825cc256e440608ffee63475a10e41ee533
VolcanicArts <VolcanicArtsBP@Gmail.com>
2021-12-21 19:39:02 -0600

    osu! login successfully

--- osu-request/Application.cs
+++ osu-request/Application.cs
@@ -1,16 +1,26 @@
 ﻿using osu.Framework;
+using osu_request.Osu;
 using osu_request.Twitch;
+using volcanicarts.osu.NET.Client;
 
 namespace osu_request
 {
     public class Application : Game
     {
+        private readonly OsuClient _osuClient;
         private readonly TwitchClientLocal _twitchClient;
 
         public Application()
         {
             _twitchClient = new TwitchClientLocal(true);
+            _osuClient = new OsuClientLocal();
+        }
+
+        protected override async void LoadAsyncComplete()
+        {
             _twitchClient.Connect();
+            await _osuClient.LoginAsync();
+            base.LoadAsyncComplete();
         }
 
         protected override bool OnExiting()
---
+++ osu-request/Osu/OsuClientLocal.cs
@@ -0,0 +1,14 @@
+﻿using volcanicarts.osu.NET.Client;
+
+namespace osu_request.Osu
+{
+    public class OsuClientLocal : OsuClient
+    {
+        private const string clientId = "";
+        private const string clientSecret = "";
+
+        public OsuClientLocal() : base(new OsuClientCredentials(clientId, clientSecret))
+        {
+        }
+    }
+}
