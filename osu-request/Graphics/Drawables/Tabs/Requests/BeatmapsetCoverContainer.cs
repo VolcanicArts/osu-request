b8122634b9650627b28e2ba258547806c23dab61
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-01-07 03:54:23 +0000

    Small UI update to BeatmapsetCoverContainer

--- osu-request/Graphics/Drawables/Tabs/Requests/BeatmapsetCoverContainer.cs
+++ osu-request/Graphics/Drawables/Tabs/Requests/BeatmapsetCoverContainer.cs
@@ -3,6 +3,7 @@ using osu.Framework.Audio.Track;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Input.Events;
 using osuTK;
@@ -31,27 +32,26 @@ namespace osu_request.Drawables
             CornerRadius = 10;
             Colour = Color4.White.Opacity(0.75f);
             EdgeEffect = OsuRequestEdgeEffects.NoShadow;
-            Child = new BackgroundSprite(_backgroundTexture)
+            Child = new Sprite
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                RelativeSizeAxes = Axes.Both,
-                Size = new Vector2(2.0f, 1.0f)
+                Texture = _backgroundTexture
             };
         }
 
         protected override bool OnHover(HoverEvent e)
         {
             _previewMp3.Restart();
-            this.ScaleTo(1.01f, 100, Easing.OutCubic);
-            TweenEdgeEffectTo(OsuRequestEdgeEffects.BasicShadow, 100, Easing.OutCubic);
+            this.MoveToY(-1.5f, 100, Easing.OutCubic);
+            TweenEdgeEffectTo(OsuRequestEdgeEffects.BasicShadowBlack, 100, Easing.OutCubic);
             return true;
         }
 
         protected override void OnHoverLost(HoverLostEvent e)
         {
             _previewMp3.Stop();
-            this.ScaleTo(1.0f, 100, Easing.InCubic);
+            this.MoveToY(0.0f, 100, Easing.InCubic);
             TweenEdgeEffectTo(OsuRequestEdgeEffects.NoShadow, 100, Easing.InCubic);
         }
     }
