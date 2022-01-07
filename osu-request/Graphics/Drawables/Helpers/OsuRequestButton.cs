f1a57049a95ca9b4fc7d14d1443336825f2260ad
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-01-07 00:19:15 +0000

    Make OsuRequestButton look 3D

--- osu-request/Graphics/Drawables/Helpers/OsuRequestButton.cs
+++ osu-request/Graphics/Drawables/Helpers/OsuRequestButton.cs
@@ -21,7 +21,7 @@ namespace osu_request.Drawables
             CornerRadius = 5;
             BorderColour = Color4.Black;
             BorderThickness = 2;
-            EdgeEffect = OsuRequestEdgeEffects.BasicShadow;
+            EdgeEffect = OsuRequestEdgeEffects.NoShadow;
 
             Children = new Drawable[]
             {
@@ -49,14 +49,30 @@ namespace osu_request.Drawables
 
         protected override bool OnMouseDown(MouseDownEvent e)
         {
+            this.MoveToY(0, 100, Easing.OutCubic);
             TweenEdgeEffectTo(OsuRequestEdgeEffects.NoShadow, 100, Easing.OutCubic);
             return true;
         }
 
         protected override void OnMouseUp(MouseUpEvent e)
         {
+            this.MoveToY(-1.5f, 100, Easing.InCubic);
             TweenEdgeEffectTo(OsuRequestEdgeEffects.BasicShadow, 100, Easing.InCubic);
             base.OnMouseUp(e);
+        }
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            this.MoveToY(-1.5f, 100, Easing.InCubic);
+            TweenEdgeEffectTo(OsuRequestEdgeEffects.BasicShadow, 100, Easing.InCubic);
+            return true;
+        }
+
+        protected override void OnHoverLost(HoverLostEvent e)
+        {
+            this.MoveToY(0, 100, Easing.OutCubic);
+            TweenEdgeEffectTo(OsuRequestEdgeEffects.NoShadow, 100, Easing.OutCubic);
+            base.OnHoverLost(e);
         }
     }
 }
