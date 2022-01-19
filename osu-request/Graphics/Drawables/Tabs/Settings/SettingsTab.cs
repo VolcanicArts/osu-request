2a23be8228bbc6c8fd53bbeec6242af05b1ca544
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-01-19 02:13:25 +0000

    Add button to login with Twitch

--- osu-request/Graphics/Drawables/Tabs/Settings/SettingsTab.cs
+++ osu-request/Graphics/Drawables/Tabs/Settings/SettingsTab.cs
@@ -1,6 +1,7 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Platform;
 using osu_request.Config;
 using osu_request.Drawables.Notifications;
 using osu_request.Websocket;
@@ -10,6 +11,9 @@ namespace osu_request.Drawables
 {
     public class SettingsTab : GenericTab
     {
+        private const string twitchLoginUrl =
+            "https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=w1j4lbtlp30d1z1whtutav6mgshzd3&redirect_uri=http://localhost/redirect&scope=user:read:email+chat:read+chat:edit+moderation:read";
+
         private OsuRequestButton _saveButton;
 
         private SettingContainer ChannelNameContainer;
@@ -25,8 +29,10 @@ namespace osu_request.Drawables
         private NotificationContainer NotificationContainer { get; set; }
 
         [BackgroundDependencyLoader]
-        private void Load()
+        private void Load(GameHost host)
         {
+            OsuRequestButton _loginWithTwitch;
+
             Children = new Drawable[]
             {
                 new Container
@@ -66,20 +72,35 @@ namespace osu_request.Drawables
                     Anchor = Anchor.BottomCentre,
                     Origin = Anchor.BottomCentre,
                     RelativeSizeAxes = Axes.X,
-                    Size = new Vector2(1.0f, 150.0f),
-                    Child = _saveButton = new OsuRequestButton
+                    Size = new Vector2(1.0f, 250.0f),
+                    Padding = new MarginPadding(50),
+                    Children = new Drawable[]
                     {
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        RelativeSizeAxes = Axes.Both,
-                        Size = new Vector2(0.2f, 0.4f),
-                        Text = "Save Settings",
-                        FontSize = 30,
-                        CornerRadius = 5
+                        _loginWithTwitch = new OsuRequestButton
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            RelativeSizeAxes = Axes.Both,
+                            Size = new Vector2(0.2f, 0.4f),
+                            Text = "Login with Twitch",
+                            FontSize = 25,
+                            CornerRadius = 5
+                        },
+                        _saveButton = new OsuRequestButton
+                        {
+                            Anchor = Anchor.BottomCentre,
+                            Origin = Anchor.BottomCentre,
+                            RelativeSizeAxes = Axes.Both,
+                            Size = new Vector2(0.2f, 0.4f),
+                            Text = "Save and Connect",
+                            FontSize = 25,
+                            CornerRadius = 5
+                        }
                     }
                 }
             };
 
+            _loginWithTwitch.OnButtonClicked += () => host.OpenUrlExternally(twitchLoginUrl);
             _saveButton.OnButtonClicked += SaveButtonClicked;
         }
 
